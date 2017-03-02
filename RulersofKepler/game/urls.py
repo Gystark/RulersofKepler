4e57305cae6a9665760f3bec34d707ac9101efe4
Viktor Taskov <2209951T@student.gla.ac.uk>
2017-03-02 19:38:07 +0000

    Added login & register templates

--- RulersofKepler/game/urls.py
+++ RulersofKepler/game/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = {
     url(r'^$', views.index, name="index"),
-	url(r'^login$', views.login, name="login"),
-	url(r'^register$', views.register, name="register"),
-	url(r'^logout$', views.logout, name="logout"),
-	url(r'^lobby/list$', views.lobbylist, name="lobbylist"),
-	url(r'^lobby/(?P<lobbyid>[0-9]+)/join$', views.lobbyjoin, name="lobbyjoin"),
-	url(r'^lobby/create$', views.lobbycreate, name="lobbycreate"),
-	url(r'^about$', views.about, name="about"),
-	url(r'^game/(?P<gameid>[0-9]+)$', views.game, name="game"),
-	url(r'^leaderboard$', views.leaderboard, name="leaderboard"),
-	url(r'^account/(?P<accountid>[0-9]+)/view$', views.accountview, name="accountview"),
-	url(r'^account/settings$', views.accountsettings, name="accountsettings"),
-	url(r'^terms-and-conditions$', views.termsandconditions, name="termsandconditions")
+    url(r'^login/$', views.login, name="login"),
+    url(r'^register/$', views.register, name="register"),
+    url(r'^logout/$', views.logout, name="logout"),
+    url(r'^lobby/list/$', views.lobbylist, name="lobbylist"),
+    url(r'^lobby/(?P<lobbyid>[0-9]+)/join/$', views.lobbyjoin, name="lobbyjoin"),
+    url(r'^lobby/create/$', views.lobbycreate, name="lobbycreate"),
+    url(r'^about/$', views.about, name="about"),
+    url(r'^game/(?P<gameid>[0-9]+)/$', views.game, name="game"),
+    url(r'^leaderboard/$', views.leaderboard, name="leaderboard"),
+    url(r'^account/(?P<accountid>[0-9]+)/view/$', views.accountview, name="accountview"),
+    url(r'^account/settings/$', views.accountsettings, name="accountsettings"),
+    url(r'^terms-and-conditions/$', views.termsandconditions, name="termsandconditions")
 }
--- RulersofKepler/game/views.py
+++ RulersofKepler/game/views.py
@@ -1,40 +1,53 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "game/index.html", {"kepler": "yess"})
 
+
 def login(request):
-	return render(request, "game/login.html", {})
+    return render(request, "game/login.html", {})
+
 
 def register(request):
-	return render(request, "game/register.html", {})
+    return render(request, "game/register.html", {})
+
 
 def logout(request):
-	pass
-	
+    pass
+
+
 def lobbylist(request):
-	return render(request, "game/lobbylist.html", {})
+    return render(request, "game/lobbylist.html", {})
+
 
 def lobbyjoin(request):
-	pass
-	
+    pass
+
+
 def lobbycreate(request):
-	return render(request, "game/lobbycreate.html", {})
-	
+    return render(request, "game/lobbycreate.html", {})
+
+
 def about(request):
-	return render(request, "game/about.html", {})
-	
+    return render(request, "game/about.html", {})
+
+
 def game(request):
-	return render(request, "game/game.html", {})
-	
+    return render(request, "game/game.html", {})
+
+
 def leaderboard(request):
-	return render(request, "game/leaderboard.html", {})
+    return render(request, "game/leaderboard.html", {})
+
 
 def accountview(request):
-	return render(request, "game/accountview.html", {})
+    return render(request, "game/accountview.html", {})
+
 
 def accountsettings(request):
-	return render(request, "game/accountsettings.html", {})
+    return render(request, "game/accountsettings.html", {})
+
 
 def termsandconditions(request):
-	return render(request, "game/termsandconditions.html", {})
+    return render(request, "game/termsandconditions.html", {})

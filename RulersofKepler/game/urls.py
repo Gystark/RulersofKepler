1482da12feffb550267646f89e4e1313bddad376
Viktor Taskov <2209951T@student.gla.ac.uk>
2017-03-05 17:47:11 +0000

    Wrote login view, modified login/register templates

--- RulersofKepler/game/urls.py
+++ RulersofKepler/game/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = {
     url(r'^$', views.index, name="index"),
-    url(r'^login/$', views.login, name="login"),
+    url(r'^login/$', views.user_login, name="login"), # change the view name bcs of conflict with Django's login()
     url(r'^register/$', views.register, name="register"),
     url(r'^logout/$', views.logout, name="logout"),
     url(r'^lobby/list/$', views.lobbylist, name="lobbylist"),
--- RulersofKepler/game/views.py
+++ RulersofKepler/game/views.py
@@ -1,16 +1,48 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
 
 def index(request):
     return render(request, "game/index.html", {"kepler": "yess"})
 
 
-def login(request):
-    return render(request, "game/login.html", {})
+# changed the name because it makes a conflict with auth.login function in Django
+def user_login(request):
+    # if submit button was clicked on
+    if request.method == 'POST':
+        # obtain info from the form; get does not throw an exception
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        # try to verify the user's credentials
+        user = authenticate(username=username, password=password) # returns none if credentials aren't valid
+        if user:
+            if user.active: # if the user was deactivated by admins - for cheating, etc.. 
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            # unsuccessful authentication - tell the user so
+            return render(request, 'game/login.html', {'invalid': "Invalid login details supplied."})
+    else:
+        return render(request, "game/login.html", {})
 
 
 def register(request):
-    return render(request, "game/register.html", {})
+    # for template
+    registered = false
+    
+    if request.method == 'POST':
+        user_name = request.POST.get("username")
+        email = request.POST.get("email")
+        passwd = request.POST.get("password")
+        # confpasswd = request.POST.get("confpassword") not sure if needed
+        
+        # .....
+        
+    else:
+        return render(request, "game/register.html", {})
 
 
 def logout(request):

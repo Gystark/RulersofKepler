5fe847e97ea49975926b4caa8521cfe43f1c0aef
Gabor Daniel Borics-Kurti <2209013b@student.gla.ac.uk>
2017-03-08 17:26:17 +0000

    Fixed issues with login.

--- RulersofKepler/game/views.py
+++ RulersofKepler/game/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
 
 def index(request):
     return render(request, "game/index.html", {"kepler": "yess"})
@@ -17,11 +19,11 @@ def user_login(request):
         # try to verify the user's credentials
         user = authenticate(username=username, password=password) # returns none if credentials aren't valid
         if user:
-            if user.active: # if the user was deactivated by admins - for cheating, etc.. 
+            if user.is_active: # if the user was deactivated by admins - for cheating, etc..
                 login(request, user)
                 return HttpResponseRedirect(reverse('index'))
             else:
-                return HttpResponse("Your Rango account is disabled.")
+                return HttpResponse("Your Rulers of Kepler account is disabled.")
         else:
             # unsuccessful authentication - tell the user so
             return render(request, 'game/login.html', {'invalid': "Invalid login details supplied."})
@@ -37,15 +39,13 @@ def register(request):
         user_name = request.POST.get("username")
         email = request.POST.get("email")
         passwd = request.POST.get("password")
-        # confpasswd = request.POST.get("confpassword") not sure if needed
-        
-        # .....
+
         
     else:
         return render(request, "game/register.html", {})
 
 
-def logout(request):
+def user_logout(request):
     pass
 
 

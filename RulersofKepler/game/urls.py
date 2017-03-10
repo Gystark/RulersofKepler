8266f83ea7d74579478a4d8bff2492b1ef88318d
Gystark <gystark@gmail.com>
2017-03-10 12:15:33 +0000

    Added registration and rewrote login, logout, etc.

--- RulersofKepler/game/urls.py
+++ RulersofKepler/game/urls.py
@@ -4,16 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^login/$', views.user_login, name="login"),
-    url(r'^register/$', views.register, name="register"),
-    url(r'^logout/$', views.user_logout, name="logout"),
     url(r'^lobby/list/$', views.lobbylist, name="lobbylist"),
     url(r'^lobby/(?P<lobbyid>[0-9]+)/join/$', views.lobbyjoin, name="lobbyjoin"),
     url(r'^lobby/create/$', views.lobbycreate, name="lobbycreate"),
     url(r'^about/$', views.about, name="about"),
     url(r'^game/(?P<gameid>[0-9]+)/$', views.game, name="game"),
     url(r'^leaderboard/$', views.leaderboard, name="leaderboard"),
-    url(r'^account/(?P<accountid>[0-9]+)/view/$', views.accountview, name="accountview"),
-    url(r'^account/settings/$', views.accountsettings, name="accountsettings"),
+    url(r'^profile/(?P<pk>[0-9]+)/$', views.ProfileView.as_view(), name="profile"),
     url(r'^terms-and-conditions/$', views.termsandconditions, name="termsandconditions")
 ]
--- RulersofKepler/game/views.py
+++ RulersofKepler/game/views.py
@@ -1,52 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
+from django.views.generic import DetailView
 
 
 def index(request):
     return render(request, "game/index.html", {"kepler": "yess"})
-
-
-# changed the name because it makes a conflict with auth.login function in Django
-def user_login(request):
-    # if submit button was clicked on
-    if request.method == 'POST':
-        # obtain info from the form; get does not throw an exception
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        
-        # try to verify the user's credentials
-        user = authenticate(username=username, password=password) # returns none if credentials aren't valid
-        if user:
-            if user.is_active: # if the user was deactivated by admins - for cheating, etc..
-                login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-            else:
-                return HttpResponse("Your Rulers of Kepler account is disabled.")
-        else:
-            # unsuccessful authentication - tell the user so
-            return render(request, 'game/login.html', {'invalid': "Invalid login details supplied."})
-    else:
-        return render(request, "game/login.html", {})
-
-
-def register(request):
-    # for template
-    registered = False
-    
-    if request.method == 'POST':
-        user_name = request.POST.get("username")
-        email = request.POST.get("email")
-        passwd = request.POST.get("password")
-
-        
-    else:
-        return render(request, "game/register.html", {})
-
-
-def user_logout(request):
-    pass
 
 
 def lobbylist(request):
@@ -73,12 +32,18 @@ def leaderboard(request):
     return render(request, "game/leaderboard.html", {})
 
 
-def accountview(request):
-    return render(request, "game/accountview.html", {})
+class ProfileView(LoginRequiredMixin, DetailView):
+    """
+    View the current user's profile
+    """
+    model = User
+    template_name = "game/profile.html"
+    permission_denied_message = "Please log in to view your profile"
+    raise_exception = True
 
 
-def accountsettings(request):
-    return render(request, "game/accountsettings.html", {})
+def accountview(request, accountid):
+    return render(request, "game/profile.html", {})
 
 
 def termsandconditions(request):

aadc1df337f658aaf561e3a0ff384cc2b5120c04
Petar Petrov <2202331P@student.gla.ac.uk>
2017-03-20 02:17:29 +0000

    It's all wizardry

--- RulersofKepler/static/game.js
+++ RulersofKepler/static/game.js
@@ -46,3 +46,94 @@ function mapMouseOver(e) {
 function mapMouseOut(e) {
     $("#map-hover-img")[0].src="";
 }
+
+var mapX=0;
+var mapY=0;
+var windowW=0;
+var windowH=0;
+var mapW=2048;
+var mapH=2048;
+var moveByX=0;
+var moveByY=0;
+window.addEventListener("load",function() {
+	$("#map")[0].style.marginLeft="0px";
+	$("#map")[0].style.marginTop="0px";
+	$("#map")[0].style.width=mapW+"px";
+	$("#map")[0].style.height=mapH+"px";
+	$("#map")[0].addEventListener("mousedown",mapMouseDown,true);
+	$("#map")[0].addEventListener("mousemove",mapMouseMove,true);
+	$("#map")[0].addEventListener("mouseout",function() {moveByX=0; moveByY=0;},true);
+	window.addEventListener("resize",windowResize,true);
+	windowW=parseInt(window.getComputedStyle($("#map-holder")[0],null).getPropertyValue("width"));
+	windowH=parseInt(window.getComputedStyle($("#map-holder")[0],null).getPropertyValue("height"));
+	setTimeout("moveMapListener();",50);
+},true);
+function moveMapListener() {
+	var x,y;
+	x=parseInt($("#map")[0].style.marginLeft);
+	y=parseInt($("#map")[0].style.marginTop);
+	x+=moveByX;
+	y+=moveByY;
+	mapMove(x,y);
+	setTimeout("moveMapListener();",50);
+}
+function mapMouseDown(event) {
+	var tx,ty;
+	tx=event.pageX-parseInt($("#map")[0].style.marginLeft);
+    ty=event.pageY-35-parseInt($("#map")[0].style.marginTop);
+	if(event.preventDefault) event.preventDefault();
+	else event.returnValue=false;
+	mapX=event.clientX;
+	mapY=event.clientY;
+	return false;
+}
+function mapMouseMove(event) {
+	var x,y,mouseX,mouseY;
+	x=parseInt($("#map")[0].style.marginLeft);
+	y=parseInt($("#map")[0].style.marginTop);
+	mouseX=event.clientX;
+	mouseY=event.clientY;
+	if(event.buttons==0) {
+		if(mouseX<50) moveByX=15+(50-mouseX);
+		else if (mouseX>windowW-50) moveByX=-15+(windowW-50-mouseX);
+		else moveByX=0;
+		
+		if(mouseY<(50+35)) moveByY=15+(50+35-mouseY);
+		else if(mouseY>windowH+35-50) moveByY=-15+(windowH+35-50-mouseY);
+		else moveByY=0;
+	}
+	if(event.preventDefault) event.preventDefault();
+	else event.returnValue=false;
+	if(event.buttons!=1) {
+		return false;
+	}
+	x+=(mouseX-mapX);
+	y+=(mouseY-mapY);
+	mapMove(x,y);
+	mapX=mouseX;
+	mapY=mouseY;
+	return false;
+}
+function windowResize(event) {
+	windowW=parseInt(window.getComputedStyle($("#map-holder")[0],null).getPropertyValue("width"));
+	windowH=parseInt(window.getComputedStyle($("#map-holder")[0],null).getPropertyValue("height"));
+	x=parseInt($("#map")[0].style.marginLeft);
+	y=parseInt($("#map")[0].style.marginTop);
+	mapMove(x,y);
+}
+function mapMove(x,y) {
+    if(windowW>=mapW)
+        x=0;
+    else {
+        if(x>0) x=0;
+        else if(x<-mapW+windowW) x=-mapW+windowW;
+    }
+    if(windowH>=mapH)
+        y=0;
+    else {
+        if(y>0) y=0;
+        else if(y<-mapH+windowH) y=-mapH+windowH;
+    }
+	$("#map")[0].style.marginLeft=x+"px";
+	$("#map")[0].style.marginTop=y+"px";
+}

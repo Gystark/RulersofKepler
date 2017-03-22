b691b55e987a7dbd8928d62b0556fe30aabddcbc
Viktor Taskov <2209951T@student.gla.ac.uk>
2017-03-22 15:50:31 +0000

    Fixed tests and added favicon

--- RulersofKepler/game/tests.py
+++ RulersofKepler/game/tests.py
@@ -4,55 +4,80 @@ from .models import Lobby, UserProfile
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
-class ModelTests(TestCase):
+
+class LobbyMethodTests(TestCase):
+    def setUp(self):
+        self.lobby_test = Lobby.objects.create(name="RandomName")
+
     def test_default_lobby_name_none(self):
         """
-        The name of the lobby should be set to Lobby 1 when given a None as its name.
+        The name of the lobby should be set to "Lobby + lobby's id" when given a None as its name.
         """
-        lobby = Lobby.objects.create(name=None)
-        self.assertEqual(lobby.name, "Lobby 1")
+        self.lobby_test.name = None
+        self.lobby_test.save()
+        self.assertEqual(self.lobby_test.name, "Lobby " + str(self.lobby_test.id))
 
     def test_default_lobby_name_empty_string(self):
         """
-        The name of the lobby should be set to Lobby 1 when given an empty string as its name.
+        The name of the lobby should be set to "Lobby + lobby's id" when given an empty string as its name.
         """
-        lobby = Lobby.objects.create(name="")
-        self.assertEqual(lobby.name, "Lobby 1")
+        self.lobby_test.name = ""
+        self.lobby_test.save()
+        self.assertEqual(self.lobby_test.name, "Lobby " + str(self.lobby_test.id))
 
     def test_default_lobby_name_preserved(self):
         """
         The name of the lobby should be preserved when it is not None or empty string.
         """
-        lobby = Lobby.objects.create(name="test_lobby")
-        self.assertEqual(lobby.name, "test_lobby")
+        self.lobby_test.name = "test_name"
+        self.lobby_test.save()
+        self.assertEqual(self.lobby_test.name, "test_name")
+
+
+class SessionMethodTests(TestCase):
+    pass
+
+
+class TerritoryMethodTests(TestCase):
+    pass
 
 
 class UserProfileMethodTests(TestCase):
+    def setUp(self):
+        """
+        set up some users and their profiles
+        """
+        self.user1 = User.objects.create_user(username="user", email="user@user.user", password="Useruser17")
+        self.user1_profile = UserProfile.objects.get_or_create(user=self.user1)[0]
+        self.user2 = User.objects.create_user(username="user1", email="user1@user1.user", password="User1user117")
+        self.user2profile = UserProfile.objects.get_or_create(user=self.user2)[0]
+
+    def tearDown(self):
+        """
+        delete previously created users
+        """
+        self.user1.delete()
+        self.user2.delete()
+
     def test_ensure_games_played_are_not_negative(self):
         """
         ensure_games_played_are_not_negative should result True for user profiles
         where games_played are zero or positive
         """
-        my_user = User.objects.create_user(username="user", email="user@user.user", password="Useruser17")
-        my_user_profile = UserProfile.objects.get_or_create(user=my_user)[0]
-        my_user_profile.games_played = -1
-        my_user_profile.games_won = 0
-        my_user_profile.save()
-        my_user.save()
-        self.assertEqual(my_user_profile.games_played >= 0, True)
+        self.user1_profile.games_played = -1
+        self.user1_profile.games_won = 0
+        self.user1_profile.save()
+        self.assertEqual(self.user1_profile.games_played >= 0, True)
 
     def test_ensure_games_won_are_not_negative(self):
         """
         ensure_games_won_are_not_negative should result True for user profiles
         where games_won are zero or positive
         """
-        my_user = User.objects.create_user(username="user", email="user@user.user", password="Useruser17")
-        my_user_profile = UserProfile.objects.get_or_create(user=my_user)[0]
-        my_user_profile.games_played = 0
-        my_user_profile.games_won = -5
-        my_user_profile.save()
-        my_user.save()
-        self.assertEqual(my_user_profile.games_won >= 0, True)
+        self.user1_profile.games_played = 0
+        self.user1_profile.games_won = -5
+        self.user1_profile.save()
+        self.assertEqual(self.user1_profile.games_won >= 0, True)
 
 
 class AboutViewTests(TestCase):
@@ -61,4 +86,3 @@ class AboutViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "This is the 'about' page")
         self.assertEqual(response.context["name"], "kepler")
-

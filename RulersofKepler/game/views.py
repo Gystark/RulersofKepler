6ca71b210acc12da1eb2cff20cec0731a2a11a27
Gabor Daniel Borics-Kurti <2209013b@student.gla.ac.uk>
2017-03-23 21:26:41 +0000

    Fixed some stuff, started making the presentation better.

--- RulersofKepler/game/views.py
+++ RulersofKepler/game/views.py
@@ -319,45 +319,6 @@ def move_army(request):
     return redirect('index')
 
 
-def get_battle_winner(defend_terr, attack_terr):
-    """
-    Get the winner in a battle between two territories
-    Also change the territory owner accordingly
-    """
-    def_score = (defend_terr.army * 1.0 + 0.15 * (
-    defend_terr.food + defend_terr.gold) + 0.1 * defend_terr.population) * uniform(0.9, 1.1)
-    att_score = (attack_terr.army * 1.0 + 0.15 * (
-    attack_terr.food + attack_terr.gold) + 0.1 * attack_terr.population) * uniform(0.9, 1.1)
-    if att_score > def_score:
-        defend_terr.change_owner(attack_terr.owner)
-        defend_terr.army /= 2
-        defend_terr.food *= 0.9
-        defend_terr.gold *= 0.85
-        defend_terr.save()
-        return attack_terr.owner.user
-    attack_terr.army /= 2
-    defend_terr.food *= 0.85
-    defend_terr.gold *= 0.83
-    attack_terr.save()
-    if defend_terr.owner != '' and defend_terr.owner is not None:
-        return defend_terr.owner.user
-    else:
-        return ''
-
-
-def get_endgame(session):
-    terr = TerritorySession.objects.filter(owner=session).count()
-    if terr == 0:
-        session.active = False
-        session.save()
-        return 'loser'
-    elif terr == 19:
-        session.active = False
-        session.save()
-        return 'winner'
-    return False
-
-
 @login_required
 def attack(request):
     """
